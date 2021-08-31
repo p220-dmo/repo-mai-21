acf6b33ff67cef152e0b33bdc223415d11fb0148
bimazatene <bim.htc21@gmail.com>
2021-08-31 22:31:07 +0200

    implimenter pgcd

--- pack-tools/src/fr/htc/tools/types/PGCD.java
+++ pack-tools/src/fr/htc/tools/types/PGCD.java
@@ -5,6 +5,19 @@ public class PGCD implements Operation {
 	@Override
 	public void process(String title) {
 		System.out.println("Je suis dans le choix : " + title);
+		System.out.print("Entrer le premier nombre : "); 
+		int n1 = scanner.nextInt();
+		System.out.print("Entrer le deuxieme nombre : ");
+		int n2 = scanner.nextInt();
+		int PGCD = 0;
+		while (n1 != n2) {
+	          if(n1 > n2)
+	                n1 = n1 - n2;
+	            else
+	                n2 = n2 - n1;
+	         	          
+	        }
+		System.out.printf("PGCD = %d", n2);
 	}
 
 }
